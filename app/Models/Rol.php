28bae2976a5e76219466d4ba1fba2f504e18506b
itser <juegosctg@gmail.com>
2026-03-31 12:57:24 -0600

    prueba roy

---
+++ app/Models/Rol.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rol extends Model
+{
+    protected $table = 'roles';
+
+    protected $fillable = ['nombre'];
+
+    public function usuarios()
+    {
+        return $this->belongsToMany(Usuario::class, 'usuario_rol', 'rol_id', 'usuario_id');
+    }
+}
